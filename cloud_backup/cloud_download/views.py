48744aebfa4a783aeee9671bec265959ae8d62dc
Kyle Thomas <mr.kyle.thomas@gmail.com>
2020-04-16 17:35:58 -0700

    namespaced template and got static files working

--- cloud_backup/cloud_download/views.py
+++ cloud_backup/cloud_download/views.py
@@ -4,12 +4,13 @@ from django.http import HttpResponseNotFound
 
 
 class Index(View):
-    template = 'index.html'
+    template = 'cloud_download/index.html'
 
     def get(self, request):
         if request.method == 'POST':
             print("post")
             print(request.body)
+            return
         elif request.method == 'GET':
             print("get")
             context = {'platforms': ['google', 'dropbox', 'aws']}
@@ -19,7 +20,7 @@ class Index(View):
             return HttpResponseNotFound('404')
 
 class Files(View):
-    template = 'files.html'
+    template = 'cloud_download/files.html'
 
     def get(self, request):
         context = {'files': ['/home', '/home/bart', '/root', 'hello.c']}
